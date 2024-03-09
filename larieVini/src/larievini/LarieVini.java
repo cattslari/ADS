c48f89cf4daa1b072908acc7e946be7209f61ac5
Larissa Silva 3 <114114544+cattslari@users.noreply.github.com>
2024-03-08 21:45:29 -0300

    Aula 08/03/2024

---
+++ larieVini/src/larievini/LarieVini.java
@@ -0,0 +1,16 @@
+
+package larievini;
+import javax.swing.JOptionPane;
+public class LarieVini {
+
+    public static void main(String[] args) {
+        String resp;
+        int idade;
+        
+        resp = JOptionPane.showInputDialog("digite sua idade");
+        System.out.println("resp:" + resp);
+              
+        
+    }
+    
+}
